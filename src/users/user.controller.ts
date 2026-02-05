a420838a576cb8432a6534f0fd077981fa173e33
victor-kipyegon-koech <victorkipyegon778@gmail.com>
2026-02-05 20:00:48 +0300

    added new functionalities

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { createUserService, getUserService } from "./user.service";
+import { createUserService, deleteUserservice, getUserService, updateUserService } from "./user.service";
 import { error } from "console";
 import { escape } from "querystring";
 
@@ -20,12 +20,12 @@ export const getUser= async(req:Request,res:Response)=>{
 export const createUser= async(req:Request,res:Response)=>{
   const {fullName,email} = req.body;
   if(!fullName || !email ){
-    res.send(400).json({error:"ALL fields are required!"});
+    res.status(400).json({error:"ALL fields are required!"});
     return;
   }try {
     const newUser = await createUserService({fullName,email})
     if ( newUser=== null){
-      res.status(500).json({message:"Failed ro create user"}) 
+      res.status(500).json({message:"Failed to create user"}) 
     }else{
       res.status(200).json(newUser)
     }
@@ -35,15 +35,45 @@ export const createUser= async(req:Request,res:Response)=>{
 }
 
 export const updateUser= async(req:Request,res:Response)=>{
-    const userId =req.params
-    const updateUser =req.body;
+      const userId =parseInt(req.params.id);
+      const {fullName,email} = req.body;
 
-    res.status(201).send(`user updated with :${updateUser.fullName}`)
-}
+      if(isNaN(userId)){
+        res.status(400).json({error:"Invalid user ID "});
+        return;
+      }
+      try {
+        const updatedUser =await updateUserService(userId,{fullName,email})
+      if(updateUser === null){ 
+        res.status(400).json({message:"failed to update user"})
+      return;
+      }else{
+        res.status(200).json(updateUser)
+      }
+       
+}catch(error:any){
+   res.status(401).json({error:error.message || "Failed to update"})
+      }
+      }  
+
 
 export const deleteUser= async(req:Request,res:Response)=>{
-    const userId =req.params.id
-     
+    const userId =parseInt(req.params.id)
+     if(isNaN(userId)){
+      res.status(400).json({error:"Invalid user ID"});
+      return;
+     }
+     try {
+      const deletedUser=await deleteUserservice(userId)
+      if(deletedUser){
+        res.status(200).json({message:"user deleted successfully"});
+      }else{
+        res.status(404).json ({message:"user not found"})
+      }
+     } catch (error:any) {
+      res.status(500).json({error:error.message || "Failed to delete user"});
+      
+     }
 
-    res.status(201).send(`user deleted with :${userId}`)
+     
 }
--- src/users/user.service.ts
+++ src/users/user.service.ts
@@ -24,3 +24,16 @@ const results = await client.query(query,values);
 return results .rows[0] || null 
 
 }
+export const updateUserService = async(userid:number,user:userCreateRequest)=>{
+ const query =` UPDATE public."userTable"
+           SET"fullName =$1."email" =$2 WHERE "userid" =$3 RETURNING * `
+  const values= [user.fullName,user.email,userid];
+  const results = await client.query(query,values);
+  return results.rows[0] || null;           
+             
+}
+export const deleteUserservice = async (userid:number):Promise<boolean> =>{
+const query =`DELETE FROM public."userTable" WHERE "userid" = $1`;
+const results = await client.query(query,[userid]);
+return (results.rowCount ?? 0)>0; //returns true if a row was deleted
+}
